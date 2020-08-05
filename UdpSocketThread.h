4df2d5b359d5e559c2a4c23c6352f54bf790974e
wavem <fenix24@naver.com>
2020-08-05 16:11:41 +0900

    [New] UDP Socket Thread Class
    - UDP Socket 을 위한 Thread 로 사용될 Class 선언.

--- UdpSocketThread.h
+++ UdpSocketThread.h
@@ -13,5 +13,34 @@ enum ThreadWorkingType
 };
 //---------------------------------------------------------------------------
 
+class CUdpSocketThread : public TThread {
+private:
+	ThreadWorkingType	m_eThreadWork;
+	SOCKET	*m_sock;
+
+protected:
+public: // Variables
+	BYTE sendBuff[UDP_SEND_BUF_SIZE];
+	BYTE* p_sendText;
+	BYTE recvBuff[UDP_RECV_BUF_SIZE];
+	//SERVERDATA recvData;
+	bool isTryingToConnect;
+	bool isConnected;
+
+public: // Functions
+	__fastcall CUdpSocketThread(SOCKET *p_sock);
+	__fastcall ~CUdpSocketThread();
+	bool __fastcall Receive();
+	void __fastcall Execute();
+	void __fastcall Stop();
+	void __fastcall Resume();
+	void __fastcall DoTerminate();
+	ThreadWorkingType __fastcall GetThreadStatus();
+};
+//---------------------------------------------------------------------------
+
+
+
+
 //---------------------------------------------------------------------------
 #endif
