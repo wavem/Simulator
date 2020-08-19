4e3f92085eeb97783f6509143384aa9518baec40
wavem <fenix24@naver.com>
2020-08-19 10:41:13 +0900

    [New] Thread Execute Routine
    - 임시로 코드를 작성하여 넣어보았음.
    - 다음 커밋에는 실제로 Thread 를 돌려 보겠음.

--- UdpSocketThread.cpp
+++ UdpSocketThread.cpp
@@ -26,6 +26,44 @@ __fastcall CUdpSocketThread::~CUdpSocketThread() {
 }
 //---------------------------------------------------------------------------
 
+void __fastcall CUdpSocketThread::Execute() {
+
+	// Common
+	UnicodeString t_Str = L"";
+	AnsiString t_AnsiStr = "";
+	int t_errno = 0;
+	int t_ConnectTryingCount = 1;
+
+	struct sockaddr_in	t_sockaddr_in;
+	memset(&t_sockaddr_in, 0, sizeof(t_sockaddr_in));
+	t_sockaddr_in.sin_family = AF_INET;
+	t_sockaddr_in.sin_addr.s_addr = inet_addr(IP_SERVER);
+	t_sockaddr_in.sin_port = htons(UDP_SERVER_PORT);
+
+	t_Str = L"Thread Start";
+	SendMessage(FormMain->Handle, MSG_LOG_FROM_THREAD, (unsigned int)&t_Str, 0x10);
+	m_eThreadWork = THREAD_RUNNING;
+
+
+	while(!Terminated) {
+		// For Thread Stop & Resume
+		if(m_eThreadWork != THREAD_RUNNING) {
+			if(m_eThreadWork == THREAD_TERMINATED) return;
+			WaitForSingleObject((void*)this->Handle, 500);
+			continue;
+		}
+
+		static int temp = 0;
+		t_Str = temp++;
+		SendMessage(FormMain->Handle, MSG_LOG_FROM_THREAD, (unsigned int)&t_Str, 0x10);
+		Sleep(1000);
+
+		if(temp == 10) break;
+	}
+
+	return;
+
+}
 
 
 
